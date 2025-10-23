38e24c9c235b6cc904d5fb636b4bf82e95ca995a
Gage Farmer <gage.farmer8641@gmail.com>
2025-10-23 13:10:40 -0400

    Created and formatted game screen (canvas)

--- doodlejump.js
+++ doodlejump.js
@@ -5,8 +5,6 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 620;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
