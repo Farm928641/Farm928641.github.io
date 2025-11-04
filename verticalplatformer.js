89c1eb2bb0c46f273b6590b7391004b67bb011f9
Connor Thorpe <connorthorpedo1@gmail.com>
2025-11-04 12:50:43 -0500

    Draw Starting Platform
    Created the placePlatforms function to place the platforms, defined the starting platform variables, and updated update() to draw the starting platform.

--- verticalplatformer.js
+++ verticalplatformer.js
@@ -8,6 +8,7 @@ let keys = {}; // This is used to track which keys are being pressed
 // Sounds
 const debugSound = new Audio("./sounds/debug.wav");
 
+// ------------ Game Logic -----------------
 window.onload = function() {
     board = this.document.getElementById("board");
     board.height = boardHeight;
@@ -17,7 +18,7 @@ window.onload = function() {
 
     //load player images
     playerRightImg = new Image();
-    playerRightImg.src = "./images/debug_player.png"
+    playerRightImg.src = "./images/debug_player.png";
     player.img = playerRightImg;
 
     // draw player
@@ -25,12 +26,19 @@ window.onload = function() {
         context.drawImage(player.img, player.x, player.y, player.width, player.height);
     }
 
+    // Load Platform Images
+    platformImg = new Image();
+    platformImg.src = "./images/debug_player.png"; //because we dont have platform art yet im just using the debug player
+
+    // Draw Platforms
+    placePlatforms();
 
     this.requestAnimationFrame(update); 
     this.addEventListener("keydown", movePlayer); //Listens for a key press that moves the player
     this.addEventListener("keyup", stopPlayer); // Listens for the keys to be unpressed that will stop player
 }
 
+// ----------- Object Definitions -------------
 
 //player
 let playerWidth = 46;
@@ -50,6 +58,15 @@ let player = {
     height : playerHeight,
 }
 
+// Platforms
+let platformArray = [];
+let platformWidth = 60;
+let platformHeight = 18;
+let platformImg;
+
+
+
+// -------- Functions -----------
 function update() {
     requestAnimationFrame(update);
     context.clearRect(0, 0, board.width, board.height); // Clears the canvas before drawing next frame
@@ -66,6 +83,14 @@ function update() {
 
     // Draw the player (currently glep smiling friends) over and over again
     context.drawImage(player.img, player.x, player.y, player.width, player.height);
+
+
+    // Draw platforms
+    for (i = 0; i < platformArray.length; i++) {
+        let currentPlatform = platformArray[i];
+        context.drawImage(currentPlatform.img, currentPlatform.x, currentPlatform.y,
+            currentPlatform.width, currentPlatform.height);
+    }
 }
 
 // Player Movement Logic
@@ -98,4 +123,21 @@ function stopPlayer(e) {
     } else {
         velocityX = 0; // only stop if neither is pressed
     }
+}
+
+
+// Platforms
+function placePlatforms() {
+    platformArray = []; // A list of our platforms
+
+    // Starting Platforms
+    let platform = {
+        img : platformImg,
+        width : platformWidth,
+        height : platformHeight,
+        x : boardWidth / 2, // Located in the middle of the screen
+        y : boardHeight - 50 
+    }
+
+    platformArray.push(platform); //Adds the starting platform to the array
 }
