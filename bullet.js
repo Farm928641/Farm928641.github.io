f8565afae040404c46995bbe2309c453004782e2
Connor Thorpe <connorthorpedo1@gmail.com>
2025-11-06 15:49:40 -0500

    Added Script to HTML and Bullet creation logic

--- bullet.js
+++ bullet.js
@@ -1,2 +1,24 @@
 // bullet.js
 // Handles bullets
+
+// Define Bullet Variables
+let bulletArray = [];
+let bulletWidth = 6;
+let bulletHeight = 12;
+let bulletSpeed = 8;
+let bulletColor = "yellow";
+
+
+
+// Create a new bullet when player attacks
+function shoot(player) {
+    const bullet = {
+        x: player.x + player.width / 2 - bulletWidth / 2,
+        y: player.y,
+        width: bulletWidth,
+        height: bulletHeight,
+        color: bulletColor,
+        speed: bulletSpeed
+    };
+    bulletArray.push(bullet); // Adds bullet to the bullet list
+}
