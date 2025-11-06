7469202dd2305533ebed4b442505a559b0d57789
Connor Thorpe <connorthorpedo1@gmail.com>
2025-11-06 15:58:13 -0500

    Created handleBulletCollisions and resetBullets
    By looping through each bullet and while looping through each enemy while checking if it collides using the detectCollisions function, we can now know if you shoot the enemy.

--- bullet.js
+++ bullet.js
@@ -22,3 +22,47 @@ function shoot(player) {
     };
     bulletArray.push(bullet); // Adds bullet to the bullet list
 }
+
+
+// Update
+function updateProjectiles(context, scrollSpeed) {
+    for (let i = bulletArray.length - 1; i >= 0; i--) { // for each bullet in bullets
+        const currentBullet = bulletArray[i];
+
+        // Move upward (relative to world)
+        currentBullet.y -= currentBullet.speed + scrollSpeed;
+
+        // Draw the bullet
+        context.fillStyle = currentBullet.color;
+        context.fillRect(currentBullet.x, currentBullet.y, currentBullet.width, currentBullet.height);
+
+        // Remove offscreen bullets
+        if (currentBullet.y + currentBullet.height < 0) {
+            bulletArray.splice(i, 1);
+        }
+    }
+}
+
+// Handles Collisions
+function handleBulletCollisions() {
+    for (let i = bulletArray.length - 1; i >= 0; i--) { // For each bullet in bullets
+        const currentBullet = bulletArray[i];
+
+        for (let j = enemyArray.length - 1; j >= 0; j--) { // For each enemy in enemies
+            const currentEnemy = enemyArray[j];
+            if (detectCollisions(p, e)) { // Checks the collision between the bullet and the enemy
+                // Remove both bullet and enemy
+                bulletArray.splice(i, 1);
+                enemyArray.splice(j, 1);
+                break;
+            }
+        }
+    }
+}
+
+
+
+// Clear all bullets when restarting
+function resetBullets() {
+    bulletArray = [];
+}
