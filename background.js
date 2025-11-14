e5639be89a663715a6b6410ee092adf44ca5a841
Connor Thorpe <connorthorpedo1@gmail.com>
2025-11-13 21:23:27 -0500

    Added script reference in index.html and resetBackground function

--- background.js
+++ background.js
@@ -1,2 +1,15 @@
 // background.js
 // Handles background scrolling
+
+let backgroundOffset = 0;
+let backgroundSpeed = 0.3;
+
+
+
+
+
+// Reset the background's position
+function resetBackground() {
+    backgroundOffset = 0;
+    board.style.backgroundPositionY = "0px"; // Resets the y-position
+}
