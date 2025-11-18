f7a28a082b79d83deeafa22a472db80eb9947311
Connor Thorpe <connorthorpedo1@gmail.com>
2025-11-18 12:05:32 -0500

    Added Initial Power variables and Shotgun image

--- powers.js
+++ powers.js
@@ -1,2 +1,15 @@
 // powers.js
 // Handles the power-up stuff
+
+// Setup
+let powerArray = [];
+let powerWidth = 48;
+let powerHeight = 48;
+let buckshotImg;
+let buckshotActive = false;
+
+// Load the power-up image
+function loadPowerImage() {
+    buckshotImg = new Image();
+    buckshotImg.src = "./images/shotgun.png";
+}
