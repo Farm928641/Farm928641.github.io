ce561a9de18552d73da12adf84b224601d2e1efe
Connor Thorpe <connorthorpedo1@gmail.com>
2025-11-18 12:17:36 -0500

    Added some helper functions

--- powers.js
+++ powers.js
@@ -13,3 +13,26 @@ function loadPowerImage() {
     buckshotImg = new Image();
     buckshotImg.src = "./images/shotgun.png";
 }
+
+// This will be called from the newPlatform() function in verticalplatformer.js
+// so that way we can get the correct x and y values
+function spawnPowerup(x, y) {
+    powerArray.push({
+        x: x,
+        y: y,
+        width: powerWidth,
+        height: powerHeight,
+        img: buckshotImg // We will have to change this to be some random power-up chooser later, for now just use buckshot
+    });
+}
+
+
+// Enable Buckshot
+function activateBuckshot() {
+    buckshotActive = true;
+}
+
+// Disable Buckshot
+function disableBuckshot() {
+    buckshotActive = false;
+}
