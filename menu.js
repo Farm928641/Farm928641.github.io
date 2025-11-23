f8cd86592be6e84ce2875cc5c804534fcbbbc103
Connor Thorpe <connorthorpedo1@gmail.com>
2025-11-23 14:17:23 -0500

    Created the draw menu function

--- menu.js
+++ menu.js
@@ -1,2 +1,32 @@
 // menu.js
 // handles the menu :o
+
+let gameState = "menu";
+let playButton = null;
+
+// Draw the menu screen
+function drawMenu(context, boardWidth, boardHeight) {
+    context.clearRect(0, 0, boardWidth, boardHeight); // Clear the screen before drawing
+
+    // Title
+    context.fillStyle = "yellow";
+    context.font = "32px sans-serif";
+    context.fillText("BOX JUMP", boardWidth / 2 - 120, 150);
+
+    // Play button dimensions
+    const btnX = boardWidth / 2 - 60;
+    const btnY = 260;
+    const btnW = 120;
+    const btnH = 50;
+
+    // Button background
+    context.fillStyle = "black";
+    context.fillRect(btnX, btnY, btnW, btnH);
+
+    // Button text
+    context.fillStyle = "yellow";
+    context.font = "24px sans-serif";
+    context.fillText("PLAY", btnX + 27, btnY + 33);
+
+    playButton = { x: btnX, y: btnY, w: btnW, h: btnH };
+}
